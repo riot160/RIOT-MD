88f179db2e7fff07a081117809356c36dd00a509
riot160 <eunicekate74@gmail.com>
2026-04-06 14:49:34 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  BOT_NAME: 'RIOT MD',
+  BOT_VERSION: '1.0.0',
+  PREFIX: '.',
+  OWNER_NUMBER: '254786600901', // ← your number without +
+  OWNER_NAME: 'Sydney',
+  SESSION_FOLDER: './auth_info',
+  AUTO_READ: true,
+  AUTO_TYPING: true,
+  AUTO_ONLINE: true,
+};
